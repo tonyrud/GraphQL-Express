2f65aba46db6b8fce8f028ec257913cf07a973c3
tonyrud <threefloyds16>
2017-10-16 14:45:19 -0400

    init repo

---
+++ schema/schema.js
@@ -0,0 +1,84 @@
+const graphql = require('graphql')
+const axios = require('axios')
+
+const {
+  GraphQLObjectType,
+  GraphQLInt,
+  GraphQLString,
+  GraphQLSchema,
+  GraphQLList,
+} = graphql
+
+const CompanyType = new GraphQLObjectType({
+  name: 'Company',
+  fields: {
+    id: {
+      type: GraphQLString,
+    },
+    name: {
+      type: GraphQLString,
+    },
+    description: {
+      type: GraphQLString,
+    },
+  },
+})
+
+const UserType = new GraphQLObjectType({
+  name: 'User',
+  fields: () => ({ // use function to create closure for UserType definition
+    id: {
+      type: GraphQLString,
+    },
+    firstName: {
+      type: GraphQLString,
+    },
+    age: {
+      type: GraphQLInt,
+    },
+    users: {
+      type: new GraphQLList(UserType),
+      resolve(parentValue, args) {
+        return axios
+          .get(`http://localhost:3000/companies/${parentValue.id}/users`)
+          .then(res => res.data) // return back data from axios
+      },
+    },
+    company: {
+      type: CompanyType,
+      resolve(parentValue, args) {
+        return axios
+          .get(`http://localhost:3000/companies/${parentValue.companyId}`)
+          .then(res => res.data) // return back data from axios
+      },
+    },
+  }),
+})
+
+const RootQuery = new GraphQLObjectType({
+  name: 'RootQueryType',
+  fields: {
+    user: {
+      type: UserType,
+      args: { id: { type: GraphQLString } },
+      resolve(parentValue, args) {
+        return axios
+          .get(`http://localhost:3000/users/${args.id}`)
+          .then(res => res.data) // return back data from axios
+      },
+    },
+    company: {
+      type: CompanyType,
+      args: { id: { type: GraphQLString } },
+      resolve(parentValue, args) {
+        return axios
+          .get(`http://localhost:3000/companies/${args.id}`)
+          .then(res => res.data)
+      },
+    },
+  },
+})
+
+module.exports = new GraphQLSchema({
+  query: RootQuery,
+})
